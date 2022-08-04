def19f2454810c581a3748d7ebe5aac2644bd33d
ibianche <inna.pomorska@gmail.com>
2022-08-04 15:24:42 +0200

    przyklad z ksiazki r4

---
+++ tax.js
@@ -0,0 +1,8 @@
+export function calculateTax (price) {
+  return Number(price) * 1.2;
+}
+
+
+export default function calcTaxAndSum(...prices) {
+  return prices.reduce((total, p) => total += calculateTax(p), 0);
+}
