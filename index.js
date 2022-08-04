1cc9d7e3e943407d2507ea55e4cc2c707db90f0b
ibianche <inna.pomorska@gmail.com>
2022-08-04 14:59:49 +0200

    zadanie z ksiazki

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-let hatPrice = 100;
-console.log(`Cena czapki: ${hatPrice}`);
+let hat = {
+  name: 'czapka',
+  price: 100,
+  getPriceIncTax(){
+    return Number(this.price) * 1.2;
+  }
+};
 
-let bootsPrice = "100";
-console.log(`Cena butów: ${bootsPrice}`);
-
-if(hatPrice === bootsPrice){
-  console.log('Ceny są takie same');
-} else {
-  console.log('Ceny są różne');
-}
-
-let totalPrice = Number(hatPrice) + Number(bootsPrice);      //number uzywamy po to by przekonwertowac string na liczbe
-console.log(`Kwota całkowita: ${totalPrice}`);
-
-let myVariable = 'Adam';
-console.log(`Typ: ${typeof myVariable}`);
-
-myVariable = 100;                                            //nadpisalam myVariable
-console.log(`Typ: ${typeof myVariable}`);
-
-let firstCity;
-let secondCity = firstCity || "Londyn";
-console.log(`Miasto: ${secondCity}`);
-
-
+console.log(`czapka: ${hat.price}, ${hat.getPriceIncTax()}`);
---
+++ test-index.js
@@ -0,0 +1,49 @@
+// let hatPrice = 100;
+// console.log(`Cena czapki: ${hatPrice}`);
+//
+// let bootsPrice = "100";
+// console.log(`Cena butów: ${bootsPrice}`);
+//
+// function sumPrices(first, second, third) {
+//   return first + second + third;
+// }
+//
+// let totalPrice = sumPrices(hatPrice, bootsPrice);
+// console.log(`Kwota całkowita: ${totalPrice}`);
+
+//
+// let hat = {
+//   name:'czapka',
+//   price: '100'
+// };
+//
+// let propertyCheck = hat.price ?? 0;
+// console.log(`Test ${propertyCheck}`);
+
+// if(hatPrice === bootsPrice){
+//   console.log('Ceny są takie same');
+// } else {
+//   console.log('Ceny są różne');
+// }
+//
+// let totalPrice = Number(hatPrice) + Number(bootsPrice);      //number uzywamy po to by przekonwertowac string na liczbe
+// console.log(`Kwota całkowita: ${totalPrice}`);
+//
+// let myVariable = 'Adam';
+// console.log(`Typ: ${typeof myVariable}`);
+//
+// myVariable = 100;                                            //nadpisalam myVariable
+// console.log(`Typ: ${typeof myVariable}`);
+//
+// let firstCity;
+// let secondCity = firstCity || "Londyn";
+// console.log(`Miasto: ${secondCity}`);
+//
+//
+// let taxRate;
+// console.log(`Stawka podatkowa: ${taxRate ?? 10}%`);
+//
+// taxRate = 0;
+// console.log(`Stawka podatkowa: ${taxRate ?? 10}%`);
+
+
